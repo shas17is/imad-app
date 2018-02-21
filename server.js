bc8c89ebbd11c6141601d8dc702332f7b55d1f75
Shashwat Mishra <36630199+shas17is@users.noreply.github.com>
2018-02-21 19:22:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -51,9 +51,9 @@ var htmltemplate =
         	</div>
 	</body>
 </html>
-	};
+	`;
 return  htmltemplate;	
-
+}
 
 	 
 	
@@ -69,8 +69,8 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/ui/article-one', function (req, res) {
-  res.send(createtemplate(article-one);
+app.get('/article-one', function (req, res) {
+  res.send(createtemplate(articleone));
 });
 app.get('/ui/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
