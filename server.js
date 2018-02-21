172679f49b31ea78bc5671bb6b033fe535496202
Shashwat Mishra <36630199+shas17is@users.noreply.github.com>
2018-02-21 19:18:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ content:
 
 
 function createtemplate(data) {
+    var title=data.title;   
+    var heading=data.heading;   
+    var date=data.date;   
+    var content=data.content;   
+
 var htmltemplate =
 `
 <html>
